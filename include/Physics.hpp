2685da60f0cfd1c869d43e9da8a1e1aad0161488
Giedre <giedre711120@gmail.com>
2022-11-19 12:15:18 +0100

    added physics i think

---
+++ include/Physics.hpp
@@ -0,0 +1,7 @@
+#include <Box2D/Box2D.h>
+
+class Physics
+{
+    public:
+        void test();
+};
---
+++ src/Physics.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Physics.hpp"
+
+void Physics::test()
+{
+    std::cout << "test physics?" << std::endl;
+}
